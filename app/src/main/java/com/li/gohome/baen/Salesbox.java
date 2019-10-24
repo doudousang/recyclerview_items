be7539a7962de6d1bd7192d08902a591e966886b
lc <693472074@qq.com>
2019-10-24 18:28:07 +0800

    recyclerview items

---
+++ app/src/main/java/com/li/gohome/baen/Salesbox.java
@@ -0,0 +1,78 @@
+package com.li.gohome.baen;
+
+/**
+ * Copyright 2019 aTool.org
+ */
+/**
+ * Auto-generated: 2019-09-12 14:44:4
+ *
+ * @author aTool.org (i@aTool.org)
+ * @website http://www.atool9.com/json2javabean.php
+ */
+public class Salesbox {
+
+    private String icon;
+    private String moreurl;
+    private CommonModel bigcard1;
+    private CommonModel bigcard2;
+    private CommonModel smallcard1;
+    private CommonModel smallcard2;
+    private CommonModel smallcard3;
+    private CommonModel smallcard4;
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setMoreurl(String moreurl) {
+        this.moreurl = moreurl;
+    }
+    public String getMoreurl() {
+        return moreurl;
+    }
+
+    public void setBigcard1(CommonModel bigcard1) {
+        this.bigcard1 = bigcard1;
+    }
+    public CommonModel getBigcard1() {
+        return bigcard1;
+    }
+
+    public void setBigcard2(CommonModel bigcard2) {
+        this.bigcard2 = bigcard2;
+    }
+    public CommonModel getBigcard2() {
+        return bigcard2;
+    }
+
+    public void setSmallcard1(CommonModel smallcard1) {
+        this.smallcard1 = smallcard1;
+    }
+    public CommonModel getSmallcard1() {
+        return smallcard1;
+    }
+
+    public void setSmallcard2(CommonModel smallcard2) {
+        this.smallcard2 = smallcard2;
+    }
+    public CommonModel getSmallcard2() {
+        return smallcard2;
+    }
+
+    public void setSmallcard3(CommonModel smallcard3) {
+        this.smallcard3 = smallcard3;
+    }
+    public CommonModel getSmallcard3() {
+        return smallcard3;
+    }
+
+    public void setSmallcard4(CommonModel smallcard4) {
+        this.smallcard4 = smallcard4;
+    }
+    public CommonModel getSmallcard4() {
+        return smallcard4;
+    }
+
+}
